2ea75310d42732bd776cd75e9eb105c9ea5d0eaa
Giacomo Nanni <gn.nanni@gmail.com>
2024-07-11 21:36:42 +0200

    cm

--- src/components/Viz.svelte
+++ src/components/Viz.svelte
@@ -7,8 +7,8 @@
 
     let canvas;
     let context;
-    let width = 1063;
-    let height = 354;
+    let width = 1063; //9cm
+    let height = 354; //3cm
 
     let margin = { top: 20, right: 10, bottom: 0, left: 10 };
 
