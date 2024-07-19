c8bd6f61611f7e113111098569e7bc7b1a58f87b
Giacomo Nanni <gn.nanni@gmail.com>
2024-07-19 10:49:13 +0200

    wetter

---
+++ src/routes/answers/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+    import { onMount } from "svelte";
+
+    async function fetchData() {
+        const res = await fetch(`/api/get`);
+        const json = await res.json();
+        return json;
+    }
+
+    let data = [];
+
+    onMount(async () => {
+        data = await fetchData();
+    });
+</script>
+
+<article>
+    {#if data.length > 0}
+        {#each data as d}
+            <p>
+                {d.answer}
+            </p>
+        {/each}
+    {/if}
+</article>
+
+<style>
+    article {
+        font-size: 18px;
+        padding: 10px;
+        color: white;
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        align-content: space-around;
+        justify-content: center;
+    }
+
+    p {
+        display: inline-block;
+        font-size: 2vw;
+        line-height: 2vw;
+        margin: 0;
+        padding: 0;
+        align-items: center;
+    }
+
+    p::after {
+        content: " - ";
+        padding-right: 10px;
+        color: gray;
+    }
+</style>
