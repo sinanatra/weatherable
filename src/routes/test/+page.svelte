0f5459b542fb4629558db1f2f1ba9129bbb92050
Giacomo Nanni <gn.nanni@gmail.com>
2024-08-07 12:46:08 +0200

    test

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,220 @@
+<script>
+    import { onMount } from "svelte";
+    import Viz from "@components/Viz.svelte";
+    import * as d3 from "d3";
+
+    async function fetchData() {
+        const res = await fetch(`/api/last`);
+        const json = await res.json();
+        return json;
+    }
+
+    async function fetchRecentData() {
+        const data = await d3.csv("/data/zku_weatherstation_main_hour.csv");
+        return data;
+    }
+
+    let data = [];
+    let guessed = [];
+
+    // $: console.log(guessed[0]);
+
+    $: guessedData = {
+        temp: guessed[0]?.range,
+        uv: guessed[0]?.range1,
+        wrain_piezo: guessed[0]?.range2,
+        humidity: guessed[0]?.range3,
+        windspeed: guessed[0]?.range4,
+
+        len: guessed[0]?.range1, //fix
+
+        curveSmooth: true
+            ? guessed[0]?.radio1 == "NW" || guessed[0]?.radio1 == "NE"
+            : false,
+
+        outline: true
+            ? guessed[0]?.radio1 == "SW" || guessed[0]?.radio1 == "SE"
+            : false,
+        mirror: true
+            ? guessed[0]?.radio == "Yes" || guessed[0]?.radio == "No"
+            : false,
+
+        lineThickness: guessed[0]?.lineThickness || 1,
+        fillThickness: guessed[0]?.fillThickness || 0.05,
+    };
+
+    // $: guessedData = {
+    //     temp: 0.4,
+    //     uv: 0.5,
+    //     weekRain: 0.4,
+    //     humidity: 0.5,
+    //     rainChance: 0.9,
+    //     curveSmooth: false,
+    //     outline: false,
+    // };
+
+    onMount(async () => {
+        guessed = await fetchData();
+        data = await fetchRecentData();
+    });
+
+    function updateGuessedData(key, value) {
+        guessedData[key] = parseFloat(value);
+    }
+</script>
+
+<article>
+    {#if guessed.length > 0}
+        <section class="data">
+            {#if data.length > 0}
+                <div>
+                    <Viz {data} {guessedData} />
+                </div>
+            {/if}
+        </section>
+        <section class="controls">
+            <label>
+                Temp:
+                <input
+                    type="range"
+                    min="0.1"
+                    max="1"
+                    step="0.1"
+                    value={guessedData.temp}
+                    on:input={(e) => updateGuessedData("temp", e.target.value)}
+                />
+                <span>{guessedData.temp}</span>
+            </label>
+            <label>
+                uv:
+                <input
+                    type="range"
+                    min="0.1"
+                    max="1"
+                    step="0.1"
+                    value={guessedData.uv}
+                    on:input={(e) => updateGuessedData("uv", e.target.value)}
+                />
+                <span>{guessedData.uv}</span>
+            </label>
+            <label>
+                weekRain:
+                <input
+                    type="range"
+                    min="0.1"
+                    max="1"
+                    step="0.1"
+                    value={guessedData.wrain_piezo}
+                    on:input={(e) =>
+                        updateGuessedData("wrain_piezo", e.target.value)}
+                />
+                <span>{guessedData.wrain_piezo}</span>
+            </label>
+            <label>
+                Humidity:
+                <input
+                    type="range"
+                    min="0.1"
+                    max="1"
+                    step="0.1"
+                    value={guessedData.humidity}
+                    on:input={(e) =>
+                        updateGuessedData("humidity", e.target.value)}
+                />
+                <span>{guessedData.humidity}</span>
+            </label>
+            <label>
+                windspeed:
+                <input
+                    type="range"
+                    min="0.1"
+                    max="1"
+                    step="0.1"
+                    value={guessedData.windspeed}
+                    on:input={(e) =>
+                        updateGuessedData("windspeed", e.target.value)}
+                />
+                <span>{guessedData.windspeed}</span>
+            </label>
+            <label>
+                Data length:
+                <input
+                    type="range"
+                    min="0.1"
+                    max="1"
+                    step="0.1"
+                    value={guessedData.len}
+                    on:input={(e) => updateGuessedData("len", e.target.value)}
+                />
+                <span>{guessedData.len}</span>
+            </label>
+            <label>
+                Curve Smooth:
+                <input
+                    type="checkbox"
+                    checked={guessedData.curveSmooth}
+                    on:change={(e) =>
+                        (guessedData.curveSmooth = e.target.checked)}
+                />
+            </label>
+            <label>
+                Outline:
+                <input
+                    type="checkbox"
+                    checked={guessedData.outline}
+                    on:change={(e) => (guessedData.outline = e.target.checked)}
+                />
+            </label>
+            <label>
+                Mirror:
+                <input
+                    type="checkbox"
+                    checked={guessedData.mirror}
+                    on:change={(e) => (guessedData.mirror = e.target.checked)}
+                />
+            </label>
+            <hr />
+            <label>
+                Line:
+                <input
+                    type="range"
+                    min="1"
+                    max="10"
+                    step="1"
+                    value={guessedData.lineThickness}
+                    on:input={(e) =>
+                        updateGuessedData("lineThickness", e.target.value)}
+                />
+                <span>{guessedData.lineThickness}</span>
+            </label>
+            <label>
+                Offset:
+                <input
+                    type="range"
+                    min="0"
+                    max="0.5"
+                    step="0.05"
+                    value={guessedData.fillThickness}
+                    on:input={(e) =>
+                        updateGuessedData("fillThickness", e.target.value)}
+                />
+                <span>{guessedData.fillThickness}</span>
+            </label>
+        </section>
+    {/if}
+</article>
+
+<style>
+    article {
+        height: 100vh;
+        padding: 40px;
+        font-size: 18px;
+    }
+
+    label {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        line-height: 18px;
+    }
+</style>
